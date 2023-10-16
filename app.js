0465c52fc74dddab1df618c1c51712aa05acbf98
Yusmin97 <tmdamsi97@gmail.com>
2023-10-16 11:07:33 +0900

    express 실험 실패 주석처리

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const fs = require('fs');
 //   'Content-Type': 'text/html; charset = utf-8'
 // };
 
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 //* 서버 생성
 const server = http.createServer((request, response) => {
