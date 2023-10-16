8ce66ac1632694acfa8d4143370c4202547ee163
Yusmin97 <tmdamsi97@gmail.com>
2023-10-16 14:27:40 +0900

    path를 통해 폴더 안에 있는 이미지 파일들의 이름을 출력해서 이미지를 읽게 한다

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const http = require('http');
 //* index.html 모듈 가져올 수 있게 file system Fs 변수 생성
 const fs = require('fs');
 
+const url = require('url');
+
+const path = require('path');
+
 // const contentType = {
 //   'Content-Type': 'text/html; charset = utf-8'
 // };
@@ -15,17 +19,22 @@ const fs = require('fs');
 
 //* 서버 생성
 const server = http.createServer((request, response) => {
+  let pageURL = request.url;
+  let pathname = url.parse(pageURL, true).pathname;
+
   if (request.method === 'GET' && request.url === '/') {
     // response.writeHead(200, contentType);
     fs.readFile('index.html', (err, data) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, {"Content-Type":"text/html; charset= utf-8"});
+        response.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8' });
         response.end(data);
       }
     });
-  } else if (request.method === 'GET' && request.url === '/style.css') {
+  }
+  //* 강사님 방식
+  else if (request.method === 'GET' && request.url === '/style.css') {
     // response.writeHead(200, contentType);
     fs.readFile('style.css', (err, data) => {
       if (err) {
@@ -35,18 +44,30 @@ const server = http.createServer((request, response) => {
         response.end(data);
       }
     });
-  } else if (request.method === 'GET' && request.url === '/dutchPay'){
+  }
+  else if (pageURL.startsWith('/public/images/')) {
+    let imageName = path.basename(request.url);
+    let imagePath = './public/images/' + imageName;
+    // response.writeHead(200, contentType);
+    fs.readFile(imagePath, (err, data) => {
+      if (err) {
+        console.log('호출 에러');
+      } else {
+        response.writeHead(200, { 'Content-Type': 'image/jpg; charset= utf-8' });
+        response.end(data);
+      }
+    });
+  } else if (request.method === 'GET' && request.url === '/dutchPay') {
     fs.readFile('dutch-pay.html', (err, data) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, {"Content-Type":"text/html; charset= utf-8"});
+        response.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8' });
         response.end(data);
       }
     });
-  } 
-  else {
-    response.writeHead(404, {"Content-Type":"text/html; charset= utf-8"});
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html; charset= utf-8' });
     // response.writeHead(404, contentType);
     response.end('<h1>요청 페이지를 찾을 수 없습니다</h1>');
   }
