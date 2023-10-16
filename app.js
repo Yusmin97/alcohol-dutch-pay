1b6f0fbd1bb764967e2d96d2b37ff47dd015faa0
Yusmin97 <tmdamsi97@gmail.com>
2023-10-16 15:57:22 +0900

    content-type 배열안 객체 변수 생성 후 지정

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const url = require('url');
 
 const path = require('path');
 
+const contentT = [
+  {'Content-Type': 'text/html; charset= utf-8'},
+  {'Content-Type': 'text/css; charset= utf-8'},
+  {'Content-Type': 'image/jpeg; charset= utf-8'}
+];
+
 // const contentType = {
 //   'Content-Type': 'text/html; charset = utf-8'
 // };
@@ -21,30 +27,31 @@ const path = require('path');
 const server = http.createServer((request, response) => {
   let pageURL = request.url;
   let pathname = url.parse(pageURL, true).pathname;
-
+//* 메인 페이지 조건문
   if (request.method === 'GET' && request.url === '/') {
     // response.writeHead(200, contentType);
     fs.readFile('index.html', (err, data) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8' });
+        response.writeHead(200, contentT[0]);
         response.end(data);
       }
     });
   }
-  //* 강사님 방식
+  //* 강사님 방식(외부 css 값 가져오는 조건문)
   else if (request.method === 'GET' && request.url === '/style.css') {
     // response.writeHead(200, contentType);
     fs.readFile('style.css', (err, data) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, {"Content-Type":"text/css; charset= utf-8"});
+        response.writeHead(200, contentT[1]);
         response.end(data);
       }
     });
   }
+  //* 폴더네 이미지 파일 가져오는 조건문
   else if (pageURL.startsWith('/public/images/')) {
     let imageName = path.basename(request.url);
     let imagePath = './public/images/' + imageName;
@@ -53,16 +60,17 @@ const server = http.createServer((request, response) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, { 'Content-Type': 'image/jpeg; charset= utf-8'});
+        response.writeHead(200, { 'Content-Type': 'image/png; charset= utf-8'});
         response.end(data);
       }
     });
+    //* 기능 페이지 제작
   } else if (request.method === 'GET' && request.url === '/dutchPay') {
     fs.readFile('dutch-pay.html', (err, data) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8' });
+        response.writeHead(200, contentT[2]);
         response.end(data);
       }
     });
